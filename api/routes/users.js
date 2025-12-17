6e0925f74230f3a54e8aa81b3e049793a1af6c03
Dylan Scott <dylanscottvrg@proton.me>
2025-12-17 10:51:34 +0000

    edited user.js and users.js model and controller + routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
-const UsersController = require("../controllers/users");
-
 const router = express.Router();
 
+const UsersController = require("../controllers/users");
+const tokenChecker = require("../middleware/tokenChecker");
+
 router.post("/", UsersController.create);
+router.get("/me", tokenChecker, UsersController.show);
+router.get("/:userId", UsersController.getUserById)
 
 module.exports = router;
