5905a4eb6bfb9155ffefbf0d0e1f622e7b93c3ba
Dominik Nowak <dominik.szymon22@gmail.com>
2025-12-18 11:30:27 +0100

    Backend for when taking a quiz

--- api/routes/quizzes.js
+++ api/routes/quizzes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const QuizzesController = require("../controllers/quiz");
 
 router.get("/", QuizzesController.getAllQuizzes);
+router.get("/:id", QuizzesController.getQuizById);
 router.post("/", QuizzesController.createQuiz);
+router.post("/:id/submit", QuizzesController.submitQuiz);
 
 module.exports = router;
