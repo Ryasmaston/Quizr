ecf3884507edf0fff52b975787f5daf5d52303dd
Matt-Kajdan <mt.kajdan@gmail.com>
2026-01-07 16:05:56 +0000

    chore: starndardise frontend pages naming convention

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,11 @@ import "./App.css";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
 import { Signup } from "./pages/Signup/Signup";
-import TakeQuizPage from "./pages/TakeQuiz/takeQuizPage";
-import CreateQuiz from "./pages/CreateQuiz/createQuizPage";
+import TakeQuizPage from "./pages/TakeQuiz/takeQuiz";
+import CreateQuiz from "./pages/CreateQuiz/createQuiz";
 import Layout from "./components/Layout";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import FriendsPage from "./pages/Friends Page/FriendsPage"
+import ProfilePage from "./pages/Profile/Profile";
+import FriendsPage from "./pages/Friends/Friends"
 
 
 // docs: https://reactrouter.com/en/main/start/overview
